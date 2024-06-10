04a7c98d72a1f2cc8ec21a4b55fb9cb5106e5e1b
Yasser <yasser.abassi.ya@gmail.com>
2024-06-10 17:01:45 +0100

    update Home Page

--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -1,12 +1,187 @@
 // Chakra imports
 import {
+  Avatar,
   Box,
+  Flex,
+  FormLabel,
+  Icon,
+  Select,
+  SimpleGrid,
+  useColorModeValue,
 } from "@chakra-ui/react";
+// Custom components
+import MiniStatistics from "components/card/MiniStatistics";
+import IconBox from "components/icons/IconBox";
 import React from "react";
+import {
+  MdBusiness,
+  MdLayers,
+  MdViewModule,
+  MdList,
+  MdAssignment,
+  MdPeople,
+} from "react-icons/md";
 
 export default function UserReports() {
+  // Chakra Color Mode
+  const brandColor = useColorModeValue("brand.500", "white");
+  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
+        gap="20px"
+        mb="20px"
+      >
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdBusiness} color={brandColor} />}
+            />
+          }
+          name="Total Companies"
+          value="120"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdLayers} color={brandColor} />}
+            />
+          }
+          name="Total Sections"
+          value="15"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdViewModule} color={brandColor} />}
+            />
+          }
+          name="Total Sub-Sections"
+          value="45"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdList} color={brandColor} />}
+            />
+          }
+          name="Total Referentials"
+          value="20"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdAssignment} color={brandColor} />}
+            />
+          }
+          name="Applicable Sections"
+          value="60"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdAssignment} color={brandColor} />}
+            />
+          }
+          name="Non-Applicable Sections"
+          value="40"
+        />
+      </SimpleGrid>
+
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }} gap="20px" mb="20px">
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdAssignment} color={brandColor} />}
+            />
+          }
+          name="User Actions This Month"
+          value="300"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdAssignment} color={brandColor} />}
+            />
+          }
+          name="Reports Generated"
+          value="50"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdPeople} color={brandColor} />}
+            />
+          }
+          name="Active Users"
+          value="30"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdPeople} color={brandColor} />}
+            />
+          }
+          name="Active Sessions"
+          value="45"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdPeople} color={brandColor} />}
+            />
+          }
+          name="Admins"
+          value="5"
+        />
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w="56px"
+              h="56px"
+              bg={boxBg}
+              icon={<Icon w="32px" h="32px" as={MdPeople} color={brandColor} />}
+            />
+          }
+          name="Compliance Officers"
+          value="25"
+        />
+      </SimpleGrid>
     </Box>
   );
 }
