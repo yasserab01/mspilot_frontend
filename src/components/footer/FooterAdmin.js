f69345121e9a5cb4a20481154ae617c44b82accf
Yasser <yasser.abassi.ya@gmail.com>
2024-06-12 11:56:01 +0100

    Update footer

--- src/components/footer/FooterAdmin.js
+++ src/components/footer/FooterAdmin.js
@@ -1,9 +1,6 @@
 import React from "react";
 import {
   Flex,
-  Link,
-  List,
-  ListItem,
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
@@ -35,63 +32,9 @@ export default function Footer() {
         {" "}
         &copy; {new Date().getFullYear()}
         <Text as='span' fontWeight='500' ms='4px'>
-          Horizon UI. All Rights Reserved. Made with love by
-          <Link
-            mx='3px'
-            color={textColor}
-            href='https://www.simmmple.com?ref=horizon-chakra-free'
-            target='_blank'
-            fontWeight='700'>
-            Simmmple!
-          </Link>
+          Symolias. All Rights Reserved.
         </Text>
       </Text>
-      <List display='flex'>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='mailto:hello@simmmple.com'>
-            Support
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://www.simmmple.com/licenses?ref=horizon-chakra-free'>
-            License
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://simmmple.com/terms-of-service?ref=horizon-chakra-free'>
-            Terms of Use
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://www.blog.simmmple.com/?ref=horizon-chakra-free'>
-            Blog
-          </Link>
-        </ListItem>
-      </List>
     </Flex>
   );
 }
--- src/components/footer/FooterAuth.js
+++ src/components/footer/FooterAuth.js
@@ -1,16 +1,12 @@
 import React from "react";
 import {
   Flex,
-  Link,
-  List,
-  ListItem,
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
 
 export default function Footer() {
   const textColor = useColorModeValue("gray.400", "white");
-  const linkColor = useColorModeValue("gray.400", "white");
 
   return (
     <Flex
@@ -35,63 +31,9 @@ export default function Footer() {
         mb={{ base: "20px", lg: "0px" }}>
         &copy; {new Date().getFullYear()}
         <Text as='span' fontWeight='500' ms='4px'>
-          Horizon UI. All Rights Reserved. Made with love by
-          <Link
-            mx='3px'
-            color={textColor}
-            href='https://www.simmmple.com?ref=horizon-chakra-free'
-            target='_blank'
-            fontWeight='700'>
-            Simmmple!
-          </Link>
+          Symolia. All Rights Reserved.
         </Text>
       </Text>
-      <List display='flex'>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={linkColor}
-            href='mailto:hello@simmmple.com?ref=horizon-chakra-free'>
-            Support
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={linkColor}
-            href='https://www.simmmple.com/licenses?ref=horizon-chakra-free'>
-            License
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={linkColor}
-            href='https://simmmple.com/terms-of-service?ref=horizon-chakra-free'>
-            Terms of Use
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link
-            fontWeight='500'
-            color={linkColor}
-            href='https://www.blog.simmmple.com/?ref=horizon-chakra-free'>
-            Blog
-          </Link>
-        </ListItem>
-      </List>
     </Flex>
   );
 }
